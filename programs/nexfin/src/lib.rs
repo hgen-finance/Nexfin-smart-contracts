d5f8ebeb3c5e86b60aaf5d65982c3ac79753f585
batphonghan <vanhungdeveloper@gmail.com>
2021-12-23 16:38:49 +0700

    Add more instructions

--- programs/nexfin/src/lib.rs
+++ programs/nexfin/src/lib.rs
@@ -46,34 +46,6 @@ pub mod nexfin {
         Ok(())
     }
 
-    /// Burn amount  * 1_000_000_000 from user_token
-    pub fn update_trove(ctx: Context<UpdateTrove>, amount: u64) -> ProgramResult {
-        let ref borrower = ctx.accounts.authority;
-        let ref mut user_token = ctx.accounts.user_token;
-        let ref mut mint_token = ctx.accounts.token_mint;
-        let ref mut trove = ctx.accounts.trove;
-
-        // update the amount to close price
-        trove.amount_to_close = (trove.amount_to_close).sub(amount);
-
-        msg!("the amount is {}", amount);
-        msg!("amount to close is {}", trove.amount_to_close);
-        msg!("Calling the token program to transfer tokens to the escrow's initializer...");
-
-        let amount_to_burn = amount * 1_000_000_000;
-        let burn_ctx = CpiContext::new(
-            ctx.accounts.token_program.clone(),
-            Burn {
-                authority: borrower.to_account_info(),
-                mint: mint_token.to_account_info(),
-                to: user_token.to_account_info(),
-            },
-        );
-        token::burn(burn_ctx, amount_to_burn)?;
-
-        Ok(())
-    }
-
     pub fn close_trove(ctx: Context<CloseTrove>) -> ProgramResult {
         let ref mut trove = ctx.accounts.trove;
         if trove.is_liquidated {
@@ -171,6 +143,254 @@ pub mod nexfin {
         }
         Ok(())
     }
+
+    pub fn redeem_coin(ctx: Context<RedeemCoin>, amount: u64) -> ProgramResult {
+        let ref mut borrower = ctx.accounts.authority;
+        let ref mut trove = ctx.accounts.trove;
+
+        if !trove.is_initialized {
+            return Err(LiquityError::TroveIsNotInitialized.into());
+        }
+        if trove.is_liquidated {
+            return Err(LiquityError::TroveAlreadyLiquidated.into());
+        }
+
+        trove.lamports_amount = trove.lamports_amount.sub(amount);
+        Ok(())
+    }
+
+    pub fn add_coin(ctx: Context<AddCoin>, amount: u64) -> ProgramResult {
+        let ref mut borrower = ctx.accounts.authority;
+        let ref mut trove = ctx.accounts.trove;
+
+        if !trove.is_initialized {
+            return Err(LiquityError::TroveIsNotInitialized.into());
+        }
+        if trove.is_liquidated {
+            return Err(LiquityError::TroveAlreadyLiquidated.into());
+        }
+
+        let ref mut temp_lamport_account = ctx.accounts.temp_lamport_account;
+
+        if temp_lamport_account.lamports() != amount {
+            return Err(LiquityError::ExpectedAmountMismatch.into());
+        }
+
+        trove.lamports_amount = trove.lamports_amount.add(amount);
+        Ok(())
+    }
+
+    pub fn add_deposit(ctx: Context<AddDeposit>, amount: u64) -> ProgramResult {
+        let ref mut depositor = ctx.accounts.authority;
+
+        let ref mut deposit = ctx.accounts.deposit;
+        let deposit_account = &deposit.to_account_info();
+        let rent = &Rent::from_account_info(deposit_account)?;
+
+        if !rent.is_exempt(deposit_account.lamports(), deposit_account.data_len()) {
+            return Err(LiquityError::NotRentExempt.into());
+        }
+
+        let ref mut temp_pda_token = ctx.accounts.user_token;
+        let ref mut temp_governance_token = ctx.accounts.user_gov_token;
+        let ref mut token_mint = ctx.accounts.token_mint;
+
+        if deposit.is_initialized {
+            deposit.token_amount = deposit.token_amount.add(amount);
+        } else {
+            deposit.is_initialized = true;
+            deposit.token_amount = amount;
+            deposit.reward_token_amount = 0;
+            deposit.reward_governance_token_amount = 0;
+            deposit.reward_coin_amount = 0;
+            deposit.bank = temp_pda_token.key();
+            deposit.governance_bank = temp_governance_token.key();
+            deposit.owner = *depositor.key;
+        }
+
+        let amount_to_burn = amount * 1_000_000_000;
+        let burn_ctx = CpiContext::new(
+            ctx.accounts.token_program.clone(),
+            Burn {
+                authority: depositor.to_account_info(),
+                mint: token_mint.to_account_info(),
+                to: temp_pda_token.to_account_info(),
+            },
+        );
+
+        msg!("Calling the token program to transfer tokens to the escrow's initializer...");
+        token::burn(burn_ctx, amount_to_burn)?;
+
+        Ok(())
+    }
+
+    // TODO: Who's  deposit owner, how to map a depositor to deposit
+    pub fn withdraw_deposit(ctx: Context<WithdrawDeposit>, amount: u64) -> ProgramResult {
+        let ref mut depositor = ctx.accounts.authority;
+        let ref mut deposit = ctx.accounts.deposit;
+
+        if amount > deposit.token_amount {
+            return Err(LiquityError::InsufficientLiquidity.into());
+        }
+
+        deposit.token_amount = deposit.token_amount.sub(amount);
+        msg!("the new deposit token amount is {}", deposit.token_amount);
+
+        Ok(())
+    }
+
+    pub fn claim_deposit_reward(ctx: Context<ClaimDepositReward>) -> ProgramResult {
+        let ref mut depositor = ctx.accounts.authority;
+        let ref mut deposit = ctx.accounts.deposit;
+
+        if *depositor.key != SYSTEM_ACCOUNT_ADDRESS {
+            return Err(ProgramError::MissingRequiredSignature);
+        }
+
+        deposit.reward_governance_token_amount = 0;
+        deposit.reward_token_amount = 0;
+        deposit.reward_coin_amount = 0;
+
+        Ok(())
+    }
+
+    pub fn receive_trove(ctx: Context<ReceiveTrove>) -> ProgramResult {
+        let ref mut sys_acc = ctx.accounts.sys_account;
+        let ref mut trove = ctx.accounts.trove;
+
+        if *sys_acc.key != SYSTEM_ACCOUNT_ADDRESS {
+            return Err(ProgramError::MissingRequiredSignature);
+        }
+
+        if trove.is_liquidated {
+            return Err(LiquityError::TroveAlreadyLiquidated.into());
+        }
+        trove.is_received = true;
+
+        Ok(())
+    }
+
+    pub fn add_deposit_reward(
+        ctx: Context<AddDepositReward>,
+        coin: u64,
+        governance: u64,
+        token: u64,
+    ) -> ProgramResult {
+        let ref mut depositor = ctx.accounts.sys_account;
+        let ref mut deposit = ctx.accounts.deposit;
+
+        if *depositor.key != SYSTEM_ACCOUNT_ADDRESS {
+            return Err(ProgramError::MissingRequiredSignature);
+        }
+
+        deposit.reward_coin_amount = deposit.reward_coin_amount.add(coin);
+        deposit.reward_governance_token_amount =
+            deposit.reward_governance_token_amount.add(governance);
+        deposit.reward_token_amount = deposit.reward_token_amount.add(token);
+
+        Ok(())
+    }
+
+    /// Burn amount  * 1_000_000_000 from user_token
+    pub fn update_trove(ctx: Context<UpdateTrove>, amount: u64) -> ProgramResult {
+        let ref borrower = ctx.accounts.authority;
+        let ref mut user_token = ctx.accounts.user_token;
+        let ref mut mint_token = ctx.accounts.token_mint;
+        let ref mut trove = ctx.accounts.trove;
+
+        // update the amount to close price
+        trove.amount_to_close = (trove.amount_to_close).sub(amount);
+
+        msg!("the amount is {}", amount);
+        msg!("amount to close is {}", trove.amount_to_close);
+        msg!("Calling the token program to transfer tokens to the escrow's initializer...");
+
+        let amount_to_burn = amount * 1_000_000_000;
+        let burn_ctx = CpiContext::new(
+            ctx.accounts.token_program.clone(),
+            Burn {
+                authority: borrower.to_account_info(),
+                mint: mint_token.to_account_info(),
+                to: user_token.to_account_info(),
+            },
+        );
+        token::burn(burn_ctx, amount_to_burn)?;
+
+        Ok(())
+    }
+}
+
+#[derive(Accounts)]
+pub struct AddDepositReward<'info> {
+    #[account(signer, mut)]
+    pub sys_account: AccountInfo<'info>,
+
+    #[account(mut)]
+    pub deposit: ProgramAccount<'info, state::Deposit>,
+}
+#[derive(Accounts)]
+pub struct ReceiveTrove<'info> {
+    #[account(signer, mut)]
+    pub sys_account: AccountInfo<'info>,
+
+    #[account(mut)]
+    pub trove: ProgramAccount<'info, state::Trove>,
+}
+
+#[derive(Accounts)]
+pub struct ClaimDepositReward<'info> {
+    #[account(signer, mut)]
+    pub authority: AccountInfo<'info>,
+
+    #[account(mut)]
+    pub deposit: ProgramAccount<'info, state::Deposit>,
+}
+
+#[derive(Accounts)]
+pub struct WithdrawDeposit<'info> {
+    #[account(signer, mut)]
+    pub authority: AccountInfo<'info>,
+
+    #[account(mut)]
+    pub deposit: ProgramAccount<'info, state::Deposit>,
+}
+
+#[derive(Accounts)]
+pub struct AddDeposit<'info> {
+    #[account(signer, mut)]
+    pub authority: AccountInfo<'info>,
+
+    #[account(init_if_needed, payer = authority, space = size_of::<state::Deposit>() + 8)]
+    pub deposit: ProgramAccount<'info, state::Deposit>,
+
+    pub rent: Sysvar<'info, Rent>,
+
+    #[account(address = spl_token::ID)]
+    pub token_program: AccountInfo<'info>,
+
+    #[account(mut)]
+    pub user_token: Account<'info, TokenAccount>,
+
+    #[account(mut)]
+    pub user_gov_token: Account<'info, TokenAccount>,
+
+    #[account(mut)]
+    pub token_mint: Account<'info, Mint>,
+
+    // TODO: need to change FE to add this
+    pub system_program: Program<'info, System>,
+}
+
+#[derive(Accounts)]
+pub struct AddCoin<'info> {
+    #[account(signer, mut)]
+    pub authority: AccountInfo<'info>,
+
+    #[account(mut)]
+    pub trove: ProgramAccount<'info, state::Trove>,
+
+    #[account(signer, mut)]
+    pub temp_lamport_account: AccountInfo<'info>,
 }
 
 #[derive(Accounts)]
@@ -182,6 +402,14 @@ pub struct WithdrawCoin<'info> {
     pub trove: ProgramAccount<'info, state::Trove>,
 }
 
+#[derive(Accounts)]
+pub struct RedeemCoin<'info> {
+    #[account(signer, mut)]
+    pub authority: AccountInfo<'info>,
+
+    #[account(mut)]
+    pub trove: ProgramAccount<'info, state::Trove>,
+}
 #[derive(Accounts)]
 pub struct UpdateTrove<'info> {
     #[account(signer, mut)]
@@ -226,6 +454,7 @@ pub struct LiquidateTrove<'info> {
     #[account(mut)]
     pub trove: ProgramAccount<'info, state::Trove>,
 
+    // TODO: ask PS if this one is system_program
     #[account(mut)]
     pub trove_owner: AccountInfo<'info>,
 }
